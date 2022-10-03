be66d7f2f422c5632e37a1fa3a3dd940254fc226
ViharevN <swix_off@mail.ru>
2022-10-03 15:04:24 +0300

    первое задание. Переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
+        //задание 1
+        double dog = 8;
+        double cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
     }
 }
