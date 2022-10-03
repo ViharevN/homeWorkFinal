2524deda9e57824ae3051c2a9b706e7eefe75965
ViharevN <swix_off@mail.ru>
2022-10-03 15:08:22 +0300

    третье задание. Переменные

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,14 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //задание 3
+
+        dog =  dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
     }
 }
